204f0f9cef7c3b457f9fc0844a00755f5c250ae6
vang3096 <vang3096@gmail.com>
2017-07-01 00:03:23 -0500

    created personal web setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,10 @@
 import { Component } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
+
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    constructor(private translate: TranslateService) {
-        translate.addLangs(['en', 'fr', 'ur']);
-        translate.setDefaultLang('en');
-        const browserLang = translate.getBrowserLang();
-        translate.use(browserLang.match(/en|fr|ur/) ? browserLang : 'en');
-    }
+  title = 'app';
 }
